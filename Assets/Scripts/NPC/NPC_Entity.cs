074785e1c3b7d893df8fb3a567726c2b2b789eea
Kai Opitz <Kai.Opitz@sennheiser.com>
2018-09-15 10:03:56 +0200

    SCript Setup

---
+++ Assets/Scripts/NPC/NPC_Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Entity : MonoBehaviour {
+
+    public float Health;
+    public float Speed;
+    public bool IsFriend;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
